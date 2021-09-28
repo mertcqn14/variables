e2cee22ee0208d31bf9d3a376c61cd6567336e16
mertcqn14 <mertcanciftcioglu704@gmail.com>
2021-09-28 15:48:34 +0300

    Initial commit

---
+++ src/variables/Main.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sutudentName="Ahmet" ;			
+        int studentAge=15 ;	          
+        System.out.println(sutudentName);	
+        sutudentName="Mertcan" ;
+	    System.out.println(studentAge) ;
+	    System.out.println(sutudentName+" yaþ "+studentAge) ;
+	    String studentNationalIdentiy="46876541231" ; 
+	
+	}
+	
+
+}
