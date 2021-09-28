d323e3c4c8f9608d51c07bebab546ba20e334450
mertcqn14 <mertcanciftcioglu704@gmail.com>
2021-09-28 16:14:42 +0300

    sdadas

--- src/variables/Main.java
+++ src/variables/Main.java
@@ -11,6 +11,17 @@ public class Main {
 	    System.out.println(studentAge) ;
 	    System.out.println(sutudentName+" yaþ "+studentAge) ;
 	    String studentNationalIdentiy="46876541231" ; 
+	    System.out.println("TC Kimlik Numarasý : "+studentNationalIdentiy); 
+	    double studentLength=1.78;
+	    System.out.println("Öðrencinin Boyu  :"+studentLength) ;
+	    boolean studentIsActive=false; 
+	    System.out.println("Öðrenci Devam :"+studentIsActive);
+	    //variablesExample
+	
+	
+	
+	
+	
 	
 	}
 	
